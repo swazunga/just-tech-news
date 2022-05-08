04b78ab31ec1e19db6d5c78f8f118d0bc69bcc00
Michael Kendrick <michael@locumsmart.net>
2022-05-08 17:40:00 -0500

    getting help

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const routes = require("./routes");
 const sequelize = require("./config/connection");
@@ -7,8 +8,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
+app.use(express.static(path.join(__dirname, "public")));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
