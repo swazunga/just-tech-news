8e1d42a85e046ee8ce667902ced9ccbcb6c7145e
Michael Kendrick <michael@locumsmart.net>
2022-05-08 21:22:37 -0500

    getting help

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 const app = express();
 const session = require("express-session");
+require("dotenv").config();
 
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
