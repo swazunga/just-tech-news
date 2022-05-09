f2a032101bad6ef794ef3a02844dd104cb74d936
Michael Kendrick <michael@locumsmart.net>
2022-05-08 19:12:15 -0500

    finished with 1

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const path = require("path");
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
-
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 const app = express();
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
