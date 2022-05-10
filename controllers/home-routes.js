fa81ed755eafdd44420b9bcee09dc0d6400538ea
Michael Kendrick <michael@locumsmart.net>
2022-05-09 22:52:56 -0500

    done with 3

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -34,7 +34,7 @@ router.get("/", (req, res) => {
   })
     .then((dbPostData) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render("homepage", { posts });
+      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
     })
     .catch((err) => {
       console.log(err);
@@ -88,7 +88,7 @@ router.get("/post/:id", (req, res) => {
         return;
       }
       const post = dbPostData.get({ plain: true });
-      res.render("single-post", { post });
+      res.render("single-post", { post, loggedIn: req.session.loggedIn });
     })
     .catch((err) => {
       console.log(err);
