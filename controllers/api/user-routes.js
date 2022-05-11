ee62e41133954e7ae4b46264f5e12262b75b56b6
Michael Kendrick <michael@locumsmart.net>
2022-05-11 13:42:19 -0500

    getting help

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { User, Post, Vote, Comment } = require("../../models");
-const withAuth = require("../utils/auth");
+const withAuth = require("../../utils/auth");
 
 router.get("/", (req, res) => {
   User.findAll({
